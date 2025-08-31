5af91e542e25d7c27c6e1e692bdc8f476f1c8ba1
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2025-08-31 22:09:53 +0000

    update settings.py >> RabbitMQ on localhost & store result

--- alx_travel_app/settings.py
+++ alx_travel_app/settings.py
@@ -120,3 +120,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# alx_travel_app celery broker 
+CELERY_BROKER_URL = 'amqp://localhost' 
+CELERY_RESULT_BACKEND = 'rpc://' 
+
